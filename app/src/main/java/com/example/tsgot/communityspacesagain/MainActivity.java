86807f7f06c905f186ea7f608ef66a16bfdf36ae
tsgoten <32545623+tsgoten@users.noreply.github.com>
2018-02-24 15:08:36 -0500

    XML Changes imported to the main activity

--- app/src/main/java/com/example/tsgot/communityspacesagain/MainActivity.java
+++ app/src/main/java/com/example/tsgot/communityspacesagain/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.tsgot.communityspacesagain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 //Test
 public class MainActivity extends AppCompatActivity {
+    TextView status;
+    Button buttonShowContacts, buttonRequestPlot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonShowContacts = findViewById(R.id.buttonShowContacts);
+        buttonRequestPlot = findViewById(R.id.buttonRequestPlot);
+
+
     }
 }
