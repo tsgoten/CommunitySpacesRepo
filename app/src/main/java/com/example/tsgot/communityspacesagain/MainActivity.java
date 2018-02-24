33ae78fc007de33a5b5b6974eb857c9df168bc1f
tsgoten <32545623+tsgoten@users.noreply.github.com>
2018-02-24 12:35:06 -0500

    first commit again

--- app/src/main/java/com/example/tsgot/communityspacesagain/MainActivity.java
+++ app/src/main/java/com/example/tsgot/communityspacesagain/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.tsgot.communityspacesagain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//Test
 public class MainActivity extends AppCompatActivity {
 
     @Override
